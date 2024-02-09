7eda99886cae994bfdf5493cd4d717bdae588066
glykoudis <g.lykoudis.code@proton.me>
2024-02-09 18:07:52 +0200

    WIP:
    - Add spring boot security and security configuration

---
+++ src/main/java/boot/spring/backend/quotes/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package boot.spring.backend.quotes.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .cors(AbstractHttpConfigurer::disable)
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(FormLoginConfigurer::disable)
+        .securityMatcher("/api/v1/quotes/**") // TODO CHECK
+        .authorizeHttpRequests(registry -> registry
+            .requestMatchers("/api/v1/quotes/**").permitAll())  // TODO CHECK
+        .httpBasic(Customizer.withDefaults())
+        .build();
+  }
+
+}
